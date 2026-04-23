d875d986574dcd306d3e19ef0e9d1704c092848a
the7ven <nkwelcorneille7@gmail.com>
2026-04-23 19:21:14 +0000

    feat: ajustement background et formatage total ticket

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,7 +2,6 @@ import { createBrowserClient } from '@supabase/ssr';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+ 
 
-// On utilise createBrowserClient pour que les cookies de session 
-// soient partagés entre le navigateur et le Middleware
 export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
